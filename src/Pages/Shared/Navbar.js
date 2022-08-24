f4f97ec2eb104a99a3c77d8e93afa82994f68327
OahidZihad <oahidzihad1@gmail.com>
2022-08-24 11:34:57 +0600

    navbar design completed

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -8,55 +8,65 @@ const Navbar = () => {
       <Link to="/" class="btn capitalize mx-1 text-white">
         Home
       </Link>
-      <Link to="/about" class="btn btn-ghost capitalize">
+      <Link to="/about" class="btn btn-ghost capitalize font-bold">
         About
       </Link>
-      <Link to="/appointment" class="btn btn-ghost capitalize">
+      <Link to="/appointment" class="btn btn-ghost capitalize font-bold">
         Appointment
       </Link>
-      <Link to="/review" class="btn btn-ghost capitalize">
+      <Link to="/review" class="btn btn-ghost capitalize font-bold">
         Reviews
       </Link>
-      <Link to="/contact" class="btn btn-ghost capitalize">
+      <Link to="/contact" class="btn btn-ghost capitalize font-bold">
         Contact Us
       </Link>
-      <Link to="/login" class="btn btn-ghost capitalize">
+      <Link to="/login" class="btn btn-ghost capitalize font-bold">
         Login
       </Link>
     </>
   );
   return (
-    <div class="navbar bg-base-100 container mx-auto">
-      <div class="navbar-start">
-        <a class="btn btn-ghost normal-case text-xl">Pritom's Chember</a>
-      </div>
-
-      <div className="navbar-end">
-        <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost md:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabindex="0"
-            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+    <div
+      className="bg-white sticky top-0 z-10"
+      style={{ backgroundColor: "#F0FBFF" }}
+    >
+      <div class="navbar container mx-auto">
+        <div class="navbar-start">
+          <Link
+            to="/"
+            class="btn btn-ghost normal-case text-xl font-bold text-3xl"
           >
-            <li>{menuItems}</li>
-          </ul>
+            Pritom's Chember
+          </Link>
         </div>
-        <div class="hidden md:flex">{menuItems}</div>
+
+        <div className="navbar-end">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost md:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabindex="0"
+              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>{menuItems}</li>
+            </ul>
+          </div>
+          <div class="hidden md:flex">{menuItems}</div>
+        </div>
       </div>
     </div>
   );
