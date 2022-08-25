b9df9684c2dff106e51240452b4261cacb308640
OahidZihad <oahidzihad1@gmail.com>
2022-08-25 11:10:48 +0600

    contact us and footer section added

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -7,27 +7,36 @@ const ContactUs = () => {
       <div className="container mx-auto">
         <div className="text-center py-5">
           <h5 className="text-primary text-lg">Contact Us</h5>
-          <h2 className="text-white text-4xl pt-2">Stay connected with us</h2>
+          <h2 className="text-white text-3xl">Stay connected with us</h2>
         </div>
-        <form className="text-center">
+        <form className="text-center py-4">
           <input
-            className="p-2 rounded-md"
+            className="p-2 rounded-md w-[400px]"
             type="text"
             placeholder="Email Address"
           />
           <br />
           <br />
-          <input className="p-2 rounded-md" type="text" placeholder="Subject" />
+          <input
+            className="p-2 rounded-md w-[400px]"
+            type="text"
+            placeholder="Subject"
+          />
           <br />
           <br />
           <textarea
-            className="p-2 rounded-md"
+            className="p-2 rounded-md w-[400px]"
             name=""
             id=""
             cols="30"
             rows="3"
             placeholder="Your Message"
           ></textarea>
+          <br />
+          <br />
+          <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">
+            Get Started
+          </button>
         </form>
       </div>
     </div>
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import footerImg from "../../../Assets/assets/images/footer.png";
+
+const Footer = () => {
+  return (
+    <section
+      className="container mx-auto"
+      style={{
+        backgroundImage: `url(${footerImg})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <footer class="p-10">
+        <div className="footer flex justify-between">
+          <div>
+            <span class="footer-title">SERVICES</span>
+            <a class="link link-hover">Emergency Checkup</a>
+            <a class="link link-hover">Monthly Checkup</a>
+            <a class="link link-hover">Weekly Checkup</a>
+            <a class="link link-hover">Deep Checkup</a>
+          </div>
+          <div>
+            <span class="footer-title">ORAL HEALTH</span>
+            <a class="link link-hover">Fluoride Treatment</a>
+            <a class="link link-hover">Cavity Filling</a>
+            <a class="link link-hover">Teath Whitening</a>
+          </div>
+          <div>
+            <span class="footer-title">OUR ADDRESS</span>
+            <a class="link link-hover">New York - 101010 Hudson</a>
+          </div>
+        </div>
+        <div className="pt-10 text-center">
+          <p>
+            Copyright © 2022 - All right reserved by <b>PRITOM'S CHEMBER</b>
+          </p>
+        </div>
+      </footer>
+    </section>
+  );
+};
+
+export default Footer;
