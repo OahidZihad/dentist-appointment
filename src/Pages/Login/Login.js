8b16bdfdaad8a33ff79aa3cda3743458fc74b8a2
OahidZihad <oahidzihad1@gmail.com>
2022-10-15 13:09:27 +0600

    login form using react hook form validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
@@ -9,11 +10,95 @@ const Login = () => {
     console.log(user);
   }
 
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="text-center text-2xl font-bold">Login</h2>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your Email"
+                class="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required",
+                  },
+                  pattern: {
+                    value:
+                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: "Provide a valid email",
+                  },
+                })}
+              />
+              <label class="label">
+                {errors.email?.type === "required" && (
+                  <p role="alert" className="text-red-500 text-sm">
+                    {errors.email.message}
+                  </p>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <p role="alert" className="text-red-500 text-sm">
+                    {errors.email.message}
+                  </p>
+                )}
+              </label>
+            </div>
+
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Password"
+                class="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is required",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Must be 6 characters or longer",
+                  },
+                })}
+              />
+              <label class="label">
+                {errors.password?.type === "required" && (
+                  <p role="alert" className="text-red-500 text-sm">
+                    {errors.password.message}
+                  </p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p role="alert" className="text-red-500 text-sm">
+                    {errors.password.message}
+                  </p>
+                )}
+              </label>
+            </div>
+            <input
+              class="btn w-full max-w-xs text-white"
+              type="submit"
+              value="LOGIN"
+            />
+          </form>
 
           <div className="divider">OR</div>
           <button
