6d3fd1544dde511e391e1a7c7c96befb4eb8cbc2
OahidZihad <oahidzihad1@gmail.com>
2022-11-08 21:13:04 +0600

    create history component in dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -5,26 +5,23 @@ const Dashboard = () => {
   return (
     <div class="drawer drawer-mobile">
       <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content flex flex-col items-center justify-center">
+      <div class="drawer-content">
         {/* <!-- Page content here --> */}
-        <h2 className="text-5xl text-purple-500">Dashboard</h2>
+        <h2 className="text-3xl font-bold text-purple-500">Dashboard</h2>
         <Outlet></Outlet>
-        <label
-          for="my-drawer-2"
-          class="btn btn-primary drawer-button lg:hidden"
-        >
-          Open drawer
-        </label>
       </div>
       <div class="drawer-side">
         <label for="my-drawer-2" class="drawer-overlay"></label>
-        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+        <ul class="menu p-4 overflow-y-auto w-60 bg-base-100 text-base-content">
           {/* <!-- Sidebar content here --> */}
           <li>
-            <Link to="/dashboard">Sidebar Item 1</Link>
+            <Link to="/dashboard">My Appointments</Link>
           </li>
           <li>
-            <Link to="/dashboard/review">Sidebar Item 2</Link>
+            <Link to="/dashboard/review">My Reviews</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/history">My History</Link>
           </li>
         </ul>
       </div>
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -52,6 +52,25 @@ const Navbar = () => {
     >
       <div class="navbar container mx-auto">
         <div class="navbar-start">
+          <label
+            for="my-drawer-2"
+            class="btn btn-ghost drawer-button lg:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
           <Link
             to="/"
             class="btn btn-ghost normal-case text-xl font-bold text-3xl"
